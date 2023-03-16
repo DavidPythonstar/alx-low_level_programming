1ca252d9c34ef0191eb30a7d7bd17f51244646d1
DavidPythonstar <davidpainzug@gmail.com>
2023-03-16 16:21:26 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- *main - prints the size of various variable variable types
- *return: 0 when suceessfully excuted 
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
-	printf("Size of a char: %1d byte(s)", sizeof(char));
-	printf("Size of an int: %1d byte(s)", sizeof(int));
-	printf("Size of a long int: %1d byte(s)", sizeof(long int));
-	printf("Size of a long long int: %1d byte(s)", sizeof(long long int));
-	ptintf("Size of a float: %1d byte(s)", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-
 }
