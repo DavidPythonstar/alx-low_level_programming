524d57272876129f776d690ace67420065fc60a5
DavidPythonstar <davidpainzug@gmail.com>
2023-03-16 16:16:32 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %d byte(s)", sizeof(char));
-	printf("Size of an int: %d byte(s)", sizeof(int));
-	printf("Size of a long int: %d byte(s)", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
-	ptintf("Size of a float: %d byte(s)", sizeof(float));
+	printf("Size of a char: %1d byte(s)", sizeof(char));
+	printf("Size of an int: %1d byte(s)", sizeof(int));
+	printf("Size of a long int: %1d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %1d byte(s)", sizeof(long long int));
+	ptintf("Size of a float: %1d byte(s)", sizeof(float));
 	return (0);
 
 }
