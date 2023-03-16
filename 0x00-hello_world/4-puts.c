deafdcc9453cffa979b287b46ea051dbdea3b661
DavidPythonstar <davidpainzug@gmail.com>
2023-03-16 14:50:24 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+*main - prints programming is like building to the stardard output
+*return;  Zero if sucessfully excuted
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
