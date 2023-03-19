5739d56e3e93e85fd4ecb5d760beabc5c507a3fa
DavidPythonstar <davidpainzug@gmail.com>
2023-03-19 12:45:18 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits of base 0
+ * Return: 0 for success 
+ */
+
+int main(void)
+{
+	int i = 0;
+	
+	while (i < 10)
+	{
+		putchar((i) + '0');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
