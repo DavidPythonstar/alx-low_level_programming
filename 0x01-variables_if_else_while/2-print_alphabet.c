1d8a8f35857fa55543abc6e39b179e1c12905bf3
DavidPythonstar <davidpainzug@gmail.com>
2023-03-19 14:21:56 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphatical letters
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
