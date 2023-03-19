bf75cc0104d25edd29359ecc07106d122d70fd32
DavidPythonstar <davidpainzug@gmail.com>
2023-03-19 15:14:16 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints the 16 digits of hex
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char hex[16] = "0123556789abcdef";
+	int i;
+
+	for (i = 0; i <= 16; i++)
+		putchar(hex[i]);
+	putchar('\n');
+	return (0);
+}
