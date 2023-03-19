1eba76652bac8692aac7d7e8b6c274cdf60ca354
DavidPythonstar <davidpainzug@gmail.com>
2023-03-19 13:57:08 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphatical letters
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
