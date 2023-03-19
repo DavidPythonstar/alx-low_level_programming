c695c9cba675c5b93eecc86f800db8e091fa82aa
DavidPythonstar <davidpainzug@gmail.com>
2023-03-19 04:59:07 -0700

    task0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - asigns a value to a variable n
+ * it ptints out whether the value is positive or negative
+ * Return: always 0 for suceess and nonzero for failure
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is postive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("0 is a zero\n");
+	return (0);
+}
