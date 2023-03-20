ec0cec962d3c83556a5bd0dbb5c8618b43eb4bc9
DavidPythonstar <davidpainzug@gmail.com>
2023-03-20 10:33:40 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints single character combination of bsse 10 digits
+ * Return: alway 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; 1 < 10; i++)
+	{
+		putchar((i) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
