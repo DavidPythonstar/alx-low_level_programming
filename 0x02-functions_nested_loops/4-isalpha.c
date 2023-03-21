c9b0a30f0587c652d7b493291e808a41639ae82d
DavidPythonstar <davidpainzug@gmail.com>
2023-03-21 14:23:04 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+ #include "main.h"
+
+/**
+ * _isalpha - tells if the insert character is lower case or upper case
+ * @c: input for a function
+ * Return: 1 if true and 0 if false
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 96)
+		return (1);
+	else
+		return (0);
+}
