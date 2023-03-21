c8d506f4ab299af77d0996e874192d065b66d8f5
DavidPythonstar <davidpainzug@gmail.com>
2023-03-21 15:55:27 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,23 +5,28 @@
  * @n: inpit for the function
  * Return: mone
  */
+
 void print_to_98(int n)
 {
-	if (n < 98)
+	int i, j;
+
+	if (n <= 98)
 	{
-		while (n < 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", n);
-			n++;
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	else if (n > 98)
-	{
-		while (n > 98)
-		{
-			printf("%d, ", n);
-			n--;
-		}
-	}
-	printf("98\n");
 }
