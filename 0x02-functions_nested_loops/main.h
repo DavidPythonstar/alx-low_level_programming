a0f3294c64233b85ec097393f06826a58a0de350
DavidPythonstar <davidpainzug@gmail.com>
2023-03-21 12:26:33 -0700

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef main_h
+#define main_h
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,6 +11,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-#include "_putchar.c"
-char _putchar(char c);
+int _putchar(char c);
 #endif
