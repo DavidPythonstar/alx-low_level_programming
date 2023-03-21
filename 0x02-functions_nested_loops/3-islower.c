25e52822cb93fb26c572c78bdf580a383f783b54
DavidPythonstar <davidpainzug@gmail.com>
2023-03-21 13:55:38 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+ #include "main.h"
+
+/**
+ * _islower - tells if the insert character is lower case or upper case
+ * @c: input for a function
+ * Return: 1 if true and 0 if false
+ */
+
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0)
+}
