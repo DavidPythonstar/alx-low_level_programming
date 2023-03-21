342451d8fcacbff747818de8a7aa06136eec7513
DavidPythonstar <davidpainzug@gmail.com>
2023-03-21 13:16:21 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all small case alphabets 10 x
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		i++;
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
