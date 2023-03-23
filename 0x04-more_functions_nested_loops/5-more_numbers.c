20b245e605cf25600fa4c082a0f800d263bd8cbe
DavidPythonstar <davidpainzug@gmail.com>
2023-03-23 14:03:08 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - print out numbers
- * Return: 0 always
+ * more_numbers - nmbr fct
+ * Return: no return
  */
 
 void more_numbers(void)
 {
+
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
