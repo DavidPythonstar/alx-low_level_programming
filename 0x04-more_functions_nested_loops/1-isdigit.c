104f5b313191b3cfb1798b58b46fa77017622c86
DavidPythonstar <davidpainzug@gmail.com>
2023-03-23 12:15:52 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - print out wgether a value us a digit or not
+ * @c: value to be checked
+ * Return: 1 for digit and 0 for none digit
+ */
+
+int _isdigit(int c)
+	if (c >= 0 && c  < 10)
+		return (1);
+	else
+		return (0);
