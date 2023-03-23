60af186af451bfd51805bf232d1746d8d0607cb0
DavidPythonstar <davidpainzug@gmail.com>
2023-03-23 13:09:35 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print a set of given numbers
- * Return: 0 always
- */
+*print_numbers - print 0123456789
+*Return: void
+*/
 
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i);
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
