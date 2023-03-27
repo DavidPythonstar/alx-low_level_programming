c6cce15fa54c2835be00f4edf886bba11373c8c9
DavidPythonstar <davidpainzug@gmail.com>
2023-03-27 04:35:54 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int_swap - swaps int in given variables
+ * @a: variabke 1
+ * @b: variable 2
+ * Return: None
+ */
+
+void swap_int(int *a, int *b)
+{
+	temp int;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+
+}
