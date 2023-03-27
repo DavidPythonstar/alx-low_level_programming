1fefa4f85a8f54b85153300a8488cf9b58d7c639
DavidPythonstar <davidpainzug@gmail.com>
2023-03-27 13:29:21 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one string at 2
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int counter = 0;
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+		counter++;
+	}
+	for (j = 0; j <= counter; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
