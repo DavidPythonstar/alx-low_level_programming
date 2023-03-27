c9623283ef86ebf5b5ca69509bb0fc2084936525
DavidPythonstar <davidpainzug@gmail.com>
2023-03-27 14:47:49 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,11 +18,11 @@ void puts_half(char *str)
 		i++;
 		counter++;
 	}
-	/*if (counter % 2 == 0)
+	if (counter % 2 == 0)
 		n = counter / 2;
-	else*/
+	else
 		n = (counter - 1) / 2;
-	for (j = n; j <= counter - 1; j++)
+	for (j = n + 1; j <= counter - 1; j++)
 	{
 		_putchar(str[j]);
 	}
