89d6b5fc0bf7884f7f3c1edb6e3770b8d5b0257b
DavidPythonstar <davidpainzug@gmail.com>
2023-03-29 15:15:47 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,19 +4,30 @@
  * _strcat - add two strings
  * @dest: first string
  * @src: second string
- * Return: concatenated string
+ * Return: concatenated stringt
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	for (j = 0; src[j] != '\0'; j++)
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+
+	j = 0;
+
+
+	while (src[j] != '\0')
+
 	{
 		dest[i] = src[j];
+		j++;
 		i++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
