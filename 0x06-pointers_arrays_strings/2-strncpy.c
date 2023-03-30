db5cccfa2b0fe7d965cb92b170a4c8ad5ac7cae4
DavidPythonstar <davidpainzug@gmail.com>
2023-03-30 04:18:25 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies from one string to another 
+ * @dest: where to copy to
+ * @src: where to copy from
+ * @n: number of values to be copied
+ * Return: copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
