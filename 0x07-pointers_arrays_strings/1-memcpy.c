00868e4afe8b10d1f09fe9d144707908b39aa953
DavidPythonstar <davidpainzug@gmail.com>
2023-04-03 12:37:41 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies mory
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of memory
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		src[i] = dest[i];
+		i++;
+	}
+}
