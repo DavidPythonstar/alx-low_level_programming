c56cf7d0c56046a2772584bdfca6b93f027a6070
DavidPythonstar <davidpainzug@gmail.com>
2023-04-03 11:51:03 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memset - fill a certain number of memory with constant character
+ * @b: character to fill the memory with
+ * @s: the block of memery
+ * @n: number of memery
+ * Return: Char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n <= 0; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
