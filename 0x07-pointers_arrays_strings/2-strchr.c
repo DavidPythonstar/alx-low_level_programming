2a391ee9c9e7f92f1dbe2fe701bce1b40a48e943
DavidPythonstar <davidpainzug@gmail.com>
2023-04-03 19:24:16 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - finds the occurance of  a given character
+ * @s: string
+ * @c: character to be found
+ * Return: point of the character occurance
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *d;
+	d = NULL;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			d = &s[i];
+			break;
+		}
+	}
+	return (d);
+}
