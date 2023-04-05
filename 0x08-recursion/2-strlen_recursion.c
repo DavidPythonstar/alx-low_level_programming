6f734171cef66e6fcdf860e0e8113e7345caf495
DavidPythonstar <davidpainzug@gmail.com>
2023-04-05 15:16:57 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,15 +5,10 @@
  * @s: string
  * Return: number od letters i  a string
  */
-
 int _strlen_recursion(char *s)
 {
-	int counter = 0;
-
-	if (*s == '\0')
-	{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
 		return (0);
-	}
-	counter++;
-	return (counter + _strlen_recursion(s) + 1);
 }
