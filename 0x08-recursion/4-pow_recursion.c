c975ab500a7d34142e64f1329cddf01eaf45ef1d
DavidPythonstar <davidpainzug@gmail.com>
2023-04-06 19:01:57 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - print the value of x raised to the power of y
+ * @x: the  number
+ * @y: the power
+ * Return: value of a number raised to another number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y != 0)
+		return (x * _pow_recursion(x, (y - 1)));
+	else if (y < 0)
+		return (-1);
+	else
+		return (1);
+}
