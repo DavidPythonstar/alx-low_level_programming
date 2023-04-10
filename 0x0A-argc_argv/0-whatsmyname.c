6a4495f3de92a5baf877ff6ba8f93477fba53e0c
DavidPythonstar <davidpainzug@gmail.com>
2023-04-10 11:10:54 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @argc: counts counts number of commandline argument
+ * @argv: an array of command line arguments
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
