ff2fd156f80af70722866cd5c20195ee40be41c3
DavidPythonstar <davidpainzug@gmail.com>
2023-04-20 13:50:43 -0700

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum;
+	unsigned int i;
 	va_list list;
+
+	va_start(list, n);
 
 	if (n == 0)
 		return (0);
 
-	va_start(list, n);
-	int i, sum = 0;
-
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(list, int);
-	}
+
 	va_end(list);
+
 	return (sum);
 }
