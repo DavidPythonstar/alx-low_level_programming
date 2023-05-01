71089f589fdbe8b666a706baf0e97e0a9d53441c
DavidPythonstar <davidpainzug@gmail.com>
2023-04-30 22:11:37 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Clears memeory of  the linkrd list
+ * @head: the beginnig of th linked list
+ * Return: void
+ */
+
+void free_listint2(listint_t *head)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head != NULL)
+	{
+		ptr = *head;
+		*head = ptr->next;
+		free(ptr);
+	}
+
+}
