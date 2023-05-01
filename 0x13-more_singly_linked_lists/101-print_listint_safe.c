deb29bf857a80ec34e67ea16e01a8c5cb504dd8e
DavidPythonstar <davidpainzug@gmail.com>
2023-05-01 08:30:20 -0700

    task101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints node of a linked list
+ * @head: ponts to the first node
+ * Return: size_t
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *list;
+	size_t counter = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		counter++;
+		list = head;
+		head = head->next;
+
+		if (list <= head)
+		{
+			printf("->[%p] %d\n",(void *)head, head->n);
+			exit(98);
+		}
+	}
+	return (counter);
+}
