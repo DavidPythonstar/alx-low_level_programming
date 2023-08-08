da429134d34aa19e8367d542815eaa0ee42e8b2f
DavidPythonstar <davidpainug@gmail.com>
2023-08-08 11:02:41 -0700

    task 01

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an arry char
+ * 
+ * @size:  size of array
+ * @c: char
+ * Return: array of char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char * ptr;
+	int unsigned i;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
