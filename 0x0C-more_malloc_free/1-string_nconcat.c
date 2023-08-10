97ab14177038faa48cf6a2c0a6c0ea0c07e3cf75
DavidPythonstar <davidpainug@gmail.com>
2023-08-10 13:14:35 -0700

    tak1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
 
@@ -11,32 +11,35 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *pointer;
-	unsigned int i, j, s1_len = 0;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		i = 0;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		for (i = 0; s1[i]; i++)
-			;
-	if (s2 == NULL)
-		j = 0;
-	else
-		for (j = 0; s2[j]; j++)
-			;
-	if (n >= j)
-		n = j;
-	pointer = malloc(sizeof(char) * (i + n + 1));
-	if (pointer == NULL)
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
-	s1_len = i;
-	for (i = 0; i < s1_len; i++)
-		pointer[i] = s1[i];
-	for (j = 0; j < n; j++)
+
+	while (i < len1)
 	{
-		pointer[i] = s2[j];
+		s[i] = s1[i];
 		i++;
 	}
-	pointer[i] = '\0';
-	return (pointer);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
