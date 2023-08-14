bc56d4a0cb5439b169acab30c0afd05fbee5e68a
DavidPythonstar <davidpainug@gmail.com>
2023-08-14 13:50:39 -0700

    task5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - freees the memory of dog pointer
+ *
+ * @d: dog point pointer 
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
