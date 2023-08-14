34ce98b93b293d053531a15bdc0c085bdadcded0
DavidPythonstar <davidpainug@gmail.com>
2023-08-14 11:56:22 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,22 @@
  * @age: age of the dog
  * @owner: dog owner
  */
-typedef struct
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
+/**
+ * dog_t - Typedef
+ *
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
-
