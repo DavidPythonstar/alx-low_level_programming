f7048a647504fc76a2160cbb9ee7ba55b7ade0ca
DavidPythonstar <davidpainug@gmail.com>
2023-08-14 11:26:23 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - creates do with following attrites
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog owner
+ */
+typedef struct
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
+
