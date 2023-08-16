339e7ef0b0439a3ce019c40d5a2cb920bbcc1bdc
DavidPythonstar <davidpainug@gmail.com>
2023-08-16 08:51:33 -0700

    task 0 and readme file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <sdio.h>
+#include "main.h"
+
+/**
+ * print_name - print the name
+ * @name: memory where name shud be printed
+ * @f: functiion pinter
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (nam != NULL && f != NULL)
+	{
+		f(name);
+	}
+	else
+		return;
+}
