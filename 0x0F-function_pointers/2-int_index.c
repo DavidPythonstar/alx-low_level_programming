bacc6f5c90b2701bb4dca25c94566d0923be506e
DavidPythonstar <davidpainug@gmail.com>
2023-08-16 11:29:06 -0700

    task1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - searches an interger 
+ * @array: to searched
+ * @size: of the array
+ * @cmp: function pointer
+ * Return: Int of the search 
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp !== NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+
+	retu
