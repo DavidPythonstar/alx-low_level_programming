088f7e2976fafbd1bcedc48fa7dabf163cf7220f
DavidPythonstar <davidpainug@gmail.com>
2023-08-16 10:09:44 -0700

    task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array == NULL || action == NULL)
+	if (!array || !action)
 		return;
 
 	for (i = 0; i < size; i++)
-
 		action(array[i]);
-
-}
