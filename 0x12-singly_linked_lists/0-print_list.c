33be125aab0bb19a3273e6ca3a2ea3aa6ba46006
DavidPythonstar <davidpainug@gmail.com>
2023-08-24 07:42:34 -0700

    task 0 first attempt

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list - prints out all elements
+ * of a linked list
+ * @h: h of the linked list
+ * Return: size_t
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%ld] %s\n", strlen(h->str), h->str);
+		count++;
+		h =  h->next;
+	}
+	return (count);
+}
