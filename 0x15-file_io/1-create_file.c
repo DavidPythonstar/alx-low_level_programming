ecf4899d2260c54c03046f67ad9cf18021a8cbb1
DavidPythonstar <davidpainug@gmail.com>
2023-09-04 20:06:27 -0700

    tsk1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(op, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (op == -1 || wrt == -1)
 		return (-1);
 
 	close(op);
