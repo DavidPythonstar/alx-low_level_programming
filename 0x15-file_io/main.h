10e658cc45e4b9e9324fc994629804984080ea23
DavidPythonstar <davidpainug@gmail.com>
2023-09-04 20:08:29 -0700

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,5 +6,6 @@ int append_text_to_file(const char *filename, char *text_content);
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stddef.h>
 #include <unistd.h>
 #endif
