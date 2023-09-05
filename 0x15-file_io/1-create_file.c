5d61ad16df27b9fe4ba5dd0ed16d5deff7fc3e9e
DavidPythonstar <davidpainug@gmail.com>
2023-09-04 19:50:02 -0700

    errorcorrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	op = open(filename, O_WRONLY | O_APPEND);
-	wrt = write(o, text_content, len);
+	wrt = write(op, text_content, len);
 
 	if (op == -1 || wrt == -1)
 		return (-1);
