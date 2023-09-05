89743592adf7b9bc93b0c526666c548d36980ba0
DavidPythonstar <davidpainug@gmail.com>
2023-09-04 20:25:43 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,31 +2,40 @@
 
 /**
  * create_file - Creates a file.
- * @filename: file tobe created
- * @text_content: file content.
- * Return: 1 for sucess.
+ * @filename: file to be created
+ * @text_content: file content
+ * Return: 1 for success, -1 for failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int op, wrt, len = 0;
+    int op, wrt, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+    if (text_content != NULL)
+    {
+        for (len = 0; text_content[len];)
+            len++;
+    }
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrt = write(op, text_content, len);
+    op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    if (op == -1)
+        return (-1);
 
-	if (op == -1 || wrt == -1)
-		return (-1);
+    if (len > 0)
+    {
+        wrt = write(op, text_content, len);
+        if (wrt == -1)
+        {
+            close(op);
+            return (-1);
+        }
+    }
 
-	close(op);
+    close(op);
 
-	return (1);
+    return (1);
 }
+
